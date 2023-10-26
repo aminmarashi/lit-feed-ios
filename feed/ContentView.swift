aa52730c2e10dfef39f03c625da2e6b00a87dce4
Amin Marashi <me@amin.codes>
2023-10-27 00:00:00 +0200

    Initial Commit

---
+++ feed/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  feed
+//
+//  Created by Amin Marashi on 27/10/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ feed/feedApp.swift
@@ -0,0 +1,17 @@
+//
+//  feedApp.swift
+//  feed
+//
+//  Created by Amin Marashi on 27/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct feedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
